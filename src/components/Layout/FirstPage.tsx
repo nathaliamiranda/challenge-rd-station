80f21ee8006ef0a9412d2ae26232a671217746bb
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-12 23:05:24 -0300

    feat: implement FirstPage component

---
+++ src/components/Layout/FirstPage.tsx
@@ -0,0 +1,13 @@
+import AboutRDStation from './AboutRDStation';
+import TextTogether from './TextTogether';
+
+function FirstPage() {
+  return (
+    <section>
+      <AboutRDStation/>
+      <TextTogether/>
+    </section>
+  )
+}
+
+export default FirstPage;
