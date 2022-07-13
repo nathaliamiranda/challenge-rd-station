c9acf2d270c1e9d74c9f1a25fd8155df82081c1c
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-12 23:05:04 -0300

    feat: implement TextTogether component

---
+++ src/components/Layout/TextTogether.tsx
@@ -0,0 +1,11 @@
+function TextTogether() {
+  return (
+    <section>
+      <h3>Estaremos juntos desde o primeiro passo!</h3>
+      <p>Se você ainda não sabe como extrair o máximo das ações digitais, não tem problema. O RD Station Marketing é mais que uma ferramenta, nos importamos com os seus resultados.
+Estaremos com você do planejamento até a prática!</p>
+    </section>
+  )
+}
+
+export default TextTogether;
