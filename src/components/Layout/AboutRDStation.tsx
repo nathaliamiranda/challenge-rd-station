8cb8fed426152045b403ab6259211a413bbd7f40
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-12 23:04:45 -0300

    feat: implement AboutRDStation component

---
+++ src/components/Layout/AboutRDStation.tsx
@@ -0,0 +1,10 @@
+function AboutRDStation() {
+  return (
+    <section>
+      <h1>Gere mais oportunidades para seu negócio</h1>
+      <p>O RD Station Marketing é um software para sua empresa fazer campanhas melhores, nutrir Leads, gerar oportunidades comerciais qualificadas e alcançar mais resultados.</p>
+    </section>
+  )
+}
+
+export default AboutRDStation;
