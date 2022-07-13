5b73a6aad384a54ee99c7c55585658eaeaa1e8d3
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-12 23:12:03 -0300

    feat: add Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header/Header';
+import Layout from '../components/Layout/Layout';
 
 const Home: NextPage = () => {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Layout/>
+    </>
   )
 }
 
