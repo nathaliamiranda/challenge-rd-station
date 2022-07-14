c5de3cfa94591541533249d12a53536ab009c48d
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-14 10:36:12 -0300

    feat: implement FirstLayout component

---
+++ src/components/Layout/FirstLayout.tsx
@@ -0,0 +1,15 @@
+import AboutRDStation from './AboutRDStation';
+import TextTogether from './TextTogether';
+import Form from './Form';
+
+function FirstLayout() {
+  return (
+    <section>
+      <AboutRDStation/>
+      <TextTogether/>
+      <Form />
+    </section>
+  )
+}
+
+export default FirstLayout;
