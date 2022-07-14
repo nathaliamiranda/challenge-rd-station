5a909671da31a96df07d50e6244bb5396fc12a96
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-14 10:38:13 -0300

    feat: implement import Layout components and Footer component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,15 @@
-import FirstPage from './FirstPage';
+import FirstPage from './FirstLayout';
+import FourthLayout from './FourthLayout';
+import SecondLayout from './SecondLayout';
+import ThirdLayout from './ThirdLayout';
 
 function Layout() {
   return (
     <main>
       <FirstPage/>
+      <SecondLayout />
+      <ThirdLayout />
+      <FourthLayout />
     </main>
   )
 }
