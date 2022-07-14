17b128b60f3dfe6fab3e5211dc18042e7aee0714
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-14 10:32:05 -0300

    feat: implement Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import Logo from "../Header/Logo";
+import SocialMidias from "./SocialMidias";
+
+function Footer() {
+  return (
+    <footer>
+      <Logo/>
+      <SocialMidias/>
+      <small>
+      © 2021 RD Station. Política de Privacidade.
+      </small>
+    </footer>
+  )
+}
+
+export default Footer;
