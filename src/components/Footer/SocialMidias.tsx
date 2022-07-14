2f97224457b5b28daec51344e11c07fa4b04aaac
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-07-14 10:31:19 -0300

    feat: implement SocialMidia component

---
+++ src/components/Footer/SocialMidias.tsx
@@ -0,0 +1,19 @@
+import Twitter from '../../styles/images/twitter.svg';
+import Facebook from '../../styles/images/facebook.svg';
+import Instagram from '../../styles/images/instagram.svg';
+import Linkedin from '../../styles/images/linkedin.svg';
+import Youtube from '../../styles/images/ytb.svg';
+
+function SocialMidias() {
+  return (
+    <div>
+      <Twitter/> 
+      <Facebook/>
+      <Instagram/>
+      <Linkedin/>
+      <Youtube/>
+    </div>
+  )
+}
+
+export default SocialMidias;
